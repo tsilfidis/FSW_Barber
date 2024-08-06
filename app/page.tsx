f162e1ee00553a1ca1cd7f07d48cb31eb61dd0fc
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2024-08-06 09:56:33 -0400

    feat: add Header components and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-"use client";
-
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Button>Teste</Button>
-  );
+    <div>
+      {/* {header} */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Tsilfidis!</h2>
+        <p>Seguda-feira, 5 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
