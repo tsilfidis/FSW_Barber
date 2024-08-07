f46d19cee3e13c7e454adac4adee1b3c91adaef8
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2024-08-07 15:40:34 -0400

    chore: add joing db services

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -17,6 +17,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   })
 
   if (!barbershop) {
