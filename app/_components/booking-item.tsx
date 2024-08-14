8ab0a68fddd6ec623d0028e16d0f8e25e1e26580
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2024-08-14 11:00:09 -0400

    feat: ensure booking item takes full width on home page

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -68,7 +68,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
   }
   return (
     <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
-      <SheetTrigger className="w-full">
+      <SheetTrigger className="w-full min-w-[90%]">
         <Card>
           <CardContent className="flex justify-between p-0">
             {/* ESQUERDA */}
